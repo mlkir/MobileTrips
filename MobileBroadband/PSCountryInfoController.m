c7f9361b3c0983b4f24332bb239d6dd938faee7c
Kiryushin <mlkir@smartsopht.com>
2012-04-13 22:14:41 +0400

    Добавлена возможность перехода из описания по стране в списки тарифов

--- MobileBroadband/PSCountryInfoController.m
+++ MobileBroadband/PSCountryInfoController.m
@@ -9,6 +9,12 @@
 #import "PSCountryInfoController.h"
 #import "Utils.h"
 #import "PSCountryController.h"
+#import "PSTariffsController.h"
+
+
+//Начало гиперссылки при возврате колбэка из JavaScript
+#define LINK_JS_CALLBACK_START_WITH @"callback:"
+
 
 @interface PSCountryInfoController ()
 
@@ -90,6 +96,62 @@
     popoverController.popoverContentSize = CGSizeMake(popoverController.popoverContentSize.width, COUNTRY_VIEW_HEIGHT);
     [PSCountryController setPopoverController:popoverController];
     [popoverController presentPopoverFromBarButtonItem:sender permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
+}
+
+
+#pragma mark -
+#pragma mark UIWebViewDelegate
+
+/* Если возникли ошибки при загрузке*/
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+	printf("\n\n************************************************************************************\n");
+	printf("%s: didFailLoadWithError \n", [[[[webView description] componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@";"]] objectAtIndex:0] UTF8String]);
+	printf("Error: %s\n", [[error description] UTF8String]);
+	printf("************************************************************************************\n\n\n");
+    
+	
+}
+
+/* Возвращает ответ (строку "result") при вызове в JavaScript: document.location = "callback:result"; в противном случае - nil */
+- (NSString *)getCallbackFromURLString:(NSString *)urlString {
+    //Проверяем на вхождение строки
+    NSRange range = [urlString rangeOfString:LINK_JS_CALLBACK_START_WITH];
+    //Если не найдена
+    if (range.location == NSNotFound && range.length == 0) return nil;
+    //Получаем значение колбэка
+    NSString *callback = [urlString substringFromIndex:(range.location + range.length)];
+    return callback;
+}
+
+/* Перед тем как загрузить страницу*/
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {	
+    
+    //Получае URL для перехода
+    NSString *urlString = [[request URL] absoluteString];    
+    //NSLog(@"URL: %@", urlString);
+    
+    //Проверяем не является ли данная гиперссылка callback от JavaScript
+    NSString *callback = [self getCallbackFromURLString:urlString];
+    if (callback != nil) {
+		//NSLog(@"CALLBACK: %@", callback);
+        //Переходим к списку тарифов
+        PSTariffsController *controller = [[[PSTariffsController alloc] initWithNibName:@"PSTariffsController" bundle:nil] autorelease];
+        [controller setCountry:_country];
+        [self.navigationController pushViewController:controller animated:YES];
+        return NO;
+    }
+        
+    return YES;  	
+}
+
+/* Перед началом загрузки страницы */
+- (void)webViewDidStartLoad:(UIWebView *)webView {			
+    
+}
+
+/* После загрузки страницы */
+- (void)webViewDidFinishLoad:(UIWebView *)webView {			
+    
 }
 
 
